ad2ebcd7b993e396768485d03d5a032dd2c3d7f9
Karolek420 <45067096+Karolek420@users.noreply.github.com>
2019-01-02 21:41:46 +0100

    Add files via upload

---
+++ create_audio_lib.py
@@ -0,0 +1,89 @@
+import pickle
+import os
+import scipy.io.wavfile as wav
+
+def audio_reader(path):
+    sampleRate, channel = wav.read(path)
+    channel = channel/32768
+    return channel, sampleRate
+
+def getData(directory, file):
+    path = directory + '/' + file
+    samples, rate = audio_reader(path)
+    position = file.find('0')
+    return samples, rate, file[position+1] + '_' + file[:position], path
+
+def save(data, name, number):
+    """
+    Changing data save format
+
+    Returns data as a list of lists in following order (oposit-alphabetical):
+
+    Name:      Traffic, Restaurant,    ...
+    Number:
+    0:          data    data
+    1:          data    data
+    2:          data    data
+    ...         ...     ...
+
+    Access elements by restructured[Number index][Name index]
+    """
+    createObj = []
+    keys = []
+    for i in range(number):
+        createObj.append([])
+        keys.append([])
+
+    for key in data:
+        createObj[int(key[0])].append(data[key])
+        position = key.find('_')
+        keys[int(key[0])].append(key[position+1:]+'0'+key[position-1])
+
+
+    file = open(name+'.p', 'wb')
+    pickle.dump(createObj, file)
+
+    fileKeys = open(name+'_keys.p', 'wb')
+    pickle.dump(keys, fileKeys)
+
+def reconstruct(filenameData):
+    """
+    Reconstructing data & attaching keys to samples
+
+    for example:
+
+    woman00: array([samples]), man00: array([samples]), ...
+
+    Access elements using keys
+    """
+    with open(filenameData+'.p', 'rb') as file:
+        data = pickle.load(file)
+    with open(filenameData+'_keys.p', 'rb') as keys:
+        keys = pickle.load(keys)
+    reconstructed = {}
+    for i in range(len(keys)):
+        for j in range(len(keys[i])):
+            reconstructed[keys[i][j]] = data[i][j]
+
+    print(reconstructed['woman00'])
+    return reconstructed
+
+def main():
+    for j in range(2):
+        samples = {}
+        fileDirectory = ''
+
+        if j<1:
+            fileDirectory = 'files/Noises'
+        else:
+            fileDirectory = 'files/Speakers'
+
+        for fileName in os.listdir(fileDirectory):
+            if fileName.endswith('.wav'):
+                data_ = getData(fileDirectory, fileName)
+                samples[data_[2]] = data_[0]
+                print(samples)
+        if j < 1:
+            save(samples, 'files/samplesNoise', 2)
+        else:
+            save(samples, 'files/samplesSpeaker', 4)
---
+++ mixing.py
@@ -0,0 +1,81 @@
+from pydub import AudioSegment
+import os
+
+
+def make_list(file_directory):
+    """ Making list of names of wave files. """
+
+    list_ = []
+    for fileName in os.listdir(file_directory):
+        if fileName.endswith('.wav'):
+            list_.append(fileName)
+    return list_
+
+
+speaker_list = make_list('Speakers')
+noise_list = make_list('Noises')
+
+
+def mix_waves(speakers, noises, snr):
+    """ Mixing speaker and noise waves. """
+
+    for i in range(len(speakers)):
+        for j in range(len(noises)):
+
+            speaker_wave = AudioSegment.from_file("Speakers/"+speakers[i])
+            noise_wave = AudioSegment.from_file("Noises/"+noises[j])
+
+            if snr == 1:
+                """ Signal-to-noise ratio: 1/1 (Speaker and noise are equally loud). """
+                mixed = speaker_wave.overlay(noise_wave)
+
+                speaker_str = speakers[i].replace(".wav", "")
+                noise_str = noises[j].replace(".wav", "")
+
+                """ Saving mixed waves in appropriate folder """
+                mixed.export("Mixed_snr_1_1/" + speaker_str + '_' + noise_str + ".wav", format='wav')
+
+            elif snr == 2:
+                """ Signal-to-noise ratio: 1/2 (Speaker is 2 times (6dB) louder than noise) """
+                noise_wave = noise_wave - 6
+
+                mixed = speaker_wave.overlay(noise_wave)
+
+                speaker_str = speakers[i].replace(".wav", "")
+                noise_str = noises[j].replace(".wav", "")
+
+                """ Saving mixed waves in appropriate folder """
+                mixed.export("Mixed_snr_1_2/" + speaker_str + '_' + noise_str + ".wav", format='wav')
+
+            elif snr == 4:
+                """ Signal-to-noise ratio: 1/4 (Speaker is 4 times (12dB) louder than noise) """
+                noise_wave = noise_wave - 12
+
+                mixed = speaker_wave.overlay(noise_wave)
+
+                speaker_str = speakers[i].replace(".wav", "")
+                noise_str = noises[j].replace(".wav", "")
+
+                """ Saving mixed waves in appropriate folder """
+                mixed.export("Mixed_snr_1_4/" + speaker_str + '_' + noise_str + ".wav", format='wav')
+
+            elif snr == 8:
+                """ Signal-to-noise ratio: 1/8 (Speaker is 8 times (18dB) louder than noise) """
+                noise_wave = noise_wave - 18
+
+                mixed = speaker_wave.overlay(noise_wave)
+
+                speaker_str = speakers[i].replace(".wav", "")
+                noise_str = noises[j].replace(".wav", "")
+
+                """ Saving mixed waves in appropriate folder """
+                mixed.export("Mixed_snr_1_8/" + speaker_str + '_' + noise_str + ".wav", format='wav')
+
+            else:
+                print("Unhandled case.")
+
+
+mix_waves(speaker_list, noise_list, 1)
+mix_waves(speaker_list, noise_list, 2)
+mix_waves(speaker_list, noise_list, 4)
+mix_waves(speaker_list, noise_list, 8)
